0da9fb59144723337568defbd024e7ff3329977b
yfung3 <yfung3@uw.edu>
2023-03-06 19:18:30 -0800

    Main screen layout partially done. Navigation drawer completed.

--- app/src/main/java/cse340/finalproject/MainActivity.java
+++ app/src/main/java/cse340/finalproject/MainActivity.java
@@ -1,14 +1,37 @@
 package cse340.finalproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout mLayout;
+    ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLayout = findViewById(R.id.main);
+        mToggle = new ActionBarDrawerToggle(
+                this, mLayout, R.string.nav_open, R.string.nav_close);
+
+        mLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)) return true;
+
+        return super.onOptionsItemSelected(item);
     }
 }
