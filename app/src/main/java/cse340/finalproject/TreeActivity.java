cba5b0cf0f31f097d859bdcdfb1b2b63bc94e7be
yfung3 <yfung3@uw.edu>
2023-03-08 17:43:33 -0800

    Navigation drawer not working???

--- app/src/main/java/cse340/finalproject/TreeActivity.java
+++ app/src/main/java/cse340/finalproject/TreeActivity.java
@@ -1,5 +1,6 @@
 package cse340.finalproject;
 
+import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,31 +9,27 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener{
+import androidx.appcompat.app.ActionBarDrawerToggle;
+
+public class TreeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout mLayout;
     ActionBarDrawerToggle mToggle;
-    NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tree);
 
         // Inflate and display our navigation drawer
         // Source: https://www.geeksforgeeks.org/navigation-drawer-in-android/
-        mLayout = findViewById(R.id.main);
+        mLayout = findViewById(R.id.tree);
         mToggle = new ActionBarDrawerToggle(
                 this, mLayout, R.string.nav_open, R.string.nav_close);
-
-        mNavigationView = findViewById(R.id.navigation_main);
-        mNavigationView.setNavigationItemSelectedListener(this);
 
         mLayout.addDrawerListener(mToggle);
         mToggle.syncState();
@@ -45,26 +42,13 @@ public class MainActivity extends AppCompatActivity
     // TODO: Change activity intent when navigation drawer buttons are clicked
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item){
-        Log.i("TAG", item.getItemId()+"");
         switch (item.getItemId()) {
-            case R.id.morse_tree:
-                Intent intent = new Intent(MainActivity.this, TreeActivity.class);
+            case R.id.main:
+                Intent intent = new Intent(TreeActivity.this, MainActivity.class);
                 startActivity(intent);
-                break;
 
         }
         mLayout.closeDrawer(GravityCompat.START);
         return true;
-    }
-
-    // TODO: Translate text to morse. We should try to simplify it by translating text
-    // straight into time spacing.
-
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item){
-        if(mToggle.onOptionsItemSelected(item)) return true;
-
-        return super.onOptionsItemSelected(item);
     }
 }
