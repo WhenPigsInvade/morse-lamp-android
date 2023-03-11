f1b09c1657a1b11f3bf9dc4cf1e88775023fba2e
yfung3 <yfung3@uw.edu>
2023-03-11 14:14:14 -0800

    FLASH LIGHT WORKING!!!!

--- app/src/main/java/cse340/finalproject/MainActivity.java
+++ app/src/main/java/cse340/finalproject/MainActivity.java
@@ -1,15 +1,25 @@
 package cse340.finalproject;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Queue;
@@ -27,6 +37,8 @@ public class MainActivity extends DrawerActivity{
         setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState, R.id.activity_main);
 
+        mHandler = new Handler(Looper.getMainLooper());
+        mQueue = new ArrayDeque<>();
 
         // Adds a-z to timing
         // Timing contains spacing values with key:value being char:timing
@@ -85,6 +97,33 @@ public class MainActivity extends DrawerActivity{
         flash.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Gets permission to use camera
+                // https://www.tutorialspoint.com/how-to-turn-on-flash-light-programmatically-in-android
+                Log.i("TAG", "FLash clicked");
+                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    Log.i("TAG", "Camera flash perms requested");
+                    // Permission is not granted
+                    String[] perm = {Manifest.permission.CAMERA};
+                    int camRequestCode = 100;
+                    ActivityCompat.requestPermissions(MainActivity.this, perm, camRequestCode);
+                }
+
+                // If phone has no camera
+                // Source: https://www.tutorialspoint.com/how-to-turn-on-flash-light-programmatically-in-android
+                if(!getApplicationContext().getPackageManager()
+                        .hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT)){
+                    Log.i("TAG", "Has not camera");
+                    return;
+                }
+
+                Log.i("TAG", "Removing previous runnable");
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    if(mHandler.hasCallbacks(runnable)){
+                        mHandler.removeCallbacks(runnable);
+                    }
+                }
                 String message = ((EditText)findViewById(R.id.textbox)).getText().toString();
                 sendToQueue(message.toLowerCase());
                 mHandler.post(runnable);
@@ -99,6 +138,9 @@ public class MainActivity extends DrawerActivity{
     // Runnable to turn on and off flashlight according to message in queue
     // Source: https://stackoverflow.com/a/40748375
 
+    // Turn on flashlight and check for permission
+    // Source: https://www.tutorialspoint.com/how-to-turn-on-flash-light-programmatically-in-android
+
     private Runnable runnable = new Runnable() {
         @Override
         public void run() {
@@ -106,9 +148,23 @@ public class MainActivity extends DrawerActivity{
             String camID = null;
             try {
                 camID = manager.getCameraIdList()[0];
+
+                //Check if flashlight is available
+                // Source: https://www.demo2s.com/android/android-cameramanager-getcameracharacteristics-nonnull-string-camerai.html#:~:text=Android%20CameraManager%20getCameraCharacteristics%20%28%40NonNull%20String%20cameraId%29%201%20Introduction,use%20Java%20CameraManager%20getCameraCharacteristics%20%28%40NonNull%20String%20cameraId%29%20
+                if(!manager.getCameraCharacteristics(camID).
+                        get(CameraCharacteristics.FLASH_INFO_AVAILABLE)){
+                    Log.i("TAG", "Flash not available");
+                    getString(R.string.no_flashlight);
+                    Toast toast = Toast.makeText(MainActivity.this,
+                            R.string.no_flashlight, Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                }
             } catch (CameraAccessException e) {
                 e.printStackTrace();
             }
+
+            Log.i("TAG", "mQueue is empty: " + mQueue.isEmpty());
             while(!mQueue.isEmpty()){
                 int time = mQueue.remove();
                 if(time > 0){
