afca2caccb81c11154b7995100e4fb31dd1cb13a
yfung3 <yfung3@uw.edu>
2023-03-11 16:17:56 -0800

    All 3 pages working

--- app/src/main/java/cse340/finalproject/AboutActivity.java
+++ app/src/main/java/cse340/finalproject/AboutActivity.java
@@ -1,11 +1,34 @@
 package cse340.finalproject;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutActivity extends DrawerActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_about);
         super.onCreate(savedInstanceState, R.id.activity_about);
+
+        // OnClickListener for github button
+        findViewById(R.id.github).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri url = Uri.parse(getString(R.string.github_link));
+                Intent intent = new Intent(Intent.ACTION_VIEW, url);
+                startActivity(intent);
+            }
+        });
+
+        // OnClickListener for donation button
+        findViewById(R.id.donation).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri url = Uri.parse(getString(R.string.paypal_link));
+                Intent intent = new Intent(Intent.ACTION_VIEW, url);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/cse340/finalproject/MainActivity.java
+++ app/src/main/java/cse340/finalproject/MainActivity.java
@@ -3,6 +3,10 @@ package cse340.finalproject;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
@@ -14,6 +18,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
@@ -25,12 +31,14 @@ import java.util.Locale;
 import java.util.Queue;
 import java.util.TreeMap;
 
-public class MainActivity extends DrawerActivity{
+public class MainActivity extends DrawerActivity implements SensorEventListener {
 
     private Handler mHandler;
     private TreeMap<Character, ArrayList<Integer>> mTiming;
     private Queue<Integer> mQueue;
-    public static final int DELAY = 120;
+    private SensorManager mSensorManager;
+    private float mBrightness;
+    public static int DELAY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,18 @@ public class MainActivity extends DrawerActivity{
 
         mHandler = new Handler(Looper.getMainLooper());
         mQueue = new ArrayDeque<>();
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        // Time delay between dits and dashes
+        DELAY = 120;
+
+        // Set up delay box
+        // Force numeric value in box
+        // Source: https://stackoverflow.com/a/42539028
+        EditText delayBox = findViewById(R.id.delay_box);
+        delayBox.setTransformationMethod(null); // Prevents our input from being scrambled
+        delayBox.setText(DELAY+"");
 
         // Adds a-z to timing
         // Timing contains spacing values with key:value being char:timing
@@ -100,13 +120,15 @@ public class MainActivity extends DrawerActivity{
                 // Gets permission to use camera
                 // https://www.tutorialspoint.com/how-to-turn-on-flash-light-programmatically-in-android
                 Log.i("TAG", "FLash clicked");
-                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                if(ContextCompat.checkSelfPermission(
+                        MainActivity.this, Manifest.permission.CAMERA)
                         != PackageManager.PERMISSION_GRANTED) {
                     Log.i("TAG", "Camera flash perms requested");
                     // Permission is not granted
                     String[] perm = {Manifest.permission.CAMERA};
                     int camRequestCode = 100;
-                    ActivityCompat.requestPermissions(MainActivity.this, perm, camRequestCode);
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this, perm, camRequestCode);
                 }
 
                 // If phone has no camera
@@ -117,18 +139,48 @@ public class MainActivity extends DrawerActivity{
                     return;
                 }
 
-                Log.i("TAG", "Removing previous runnable");
+                // Sends warning if environment is too bright
+                int brightnessThreshold = 15000; // In lux
+                if(mBrightness > brightnessThreshold) {
+                    Toast toast = Toast.makeText(MainActivity.this,
+                            R.string.brightness_warning, Toast.LENGTH_SHORT);
+                }
 
+                // Stop flashlight if it is still running
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                     if(mHandler.hasCallbacks(runnable)){
+                        Log.i("TAG", "Removing previous runnable");
                         mHandler.removeCallbacks(runnable);
                     }
                 }
+
+                // Grab number from delayBox and set it as the new delay
+                DELAY = Integer.parseInt(delayBox.getText().toString());
+
                 String message = ((EditText)findViewById(R.id.textbox)).getText().toString();
                 sendToQueue(message.toLowerCase());
                 mHandler.post(runnable);
             }
         });
+
+        // Inflate SOS button
+        findViewById(R.id.sos).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((EditText)findViewById(R.id.textbox)).setText(R.string.sos);
+            }
+        });
+
+        findViewById(R.id.repeat).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(MainActivity.this,
+                        R.string.premium, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+
     }
 
     // Reminder to add activity to android manifest
@@ -204,6 +256,28 @@ public class MainActivity extends DrawerActivity{
 
     }
 
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mSensorManager.registerListener(this,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT), DELAY);
+    }
 
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        TextView txt = findViewById(R.id.brightness);
+        txt.setText(getString(R.string.brightness, event.values[0]));
+        mBrightness = event.values[0];
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
